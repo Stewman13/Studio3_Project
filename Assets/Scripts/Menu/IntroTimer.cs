48c08a04aa6592847188206bed8259b4adfefc15
Stewman13 <110367@student.sae.edu.au>
2015-12-09 00:35:41 +1000

    async in intro

--- Assets/Scripts/Menu/IntroTimer.cs
+++ Assets/Scripts/Menu/IntroTimer.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class IntroTimer : MonoBehaviour {
 
+	AsyncOperation async;
+
 	// Use this for initialization
 	void Start () {
 		StartCoroutine(WaitAndPrint(3.0F));
+
 	}
 	
 	// Update is called once per frame
@@ -14,7 +17,11 @@ public class IntroTimer : MonoBehaviour {
 	}
 
 	IEnumerator WaitAndPrint(float waitTime) {
+		Debug.LogWarning("ASYNC LOAD STARTED - " +
+		                 "DO NOT EXIT PLAY MODE UNTIL SCENE LOADS... UNITY WILL CRASH");
+		async = Application.LoadLevelAsync(Application.loadedLevel+1);
+		async.allowSceneActivation = false;
 		yield return new WaitForSeconds(waitTime);
-		Application.LoadLevel (Application.loadedLevel + 1);
+		async.allowSceneActivation = true;
 	}
 }
